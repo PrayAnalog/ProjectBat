d4831ac2065d2bb771613cc78d05f0c9b17830e7
prayanalog <prayanalog@gmail.com>
2017-07-14 18:43:55 +0900

    kakao talk login button inserted

---
+++ ProjectBat/BrigingHeader.h
@@ -0,0 +1,15 @@
+//
+//  BrigingHeader.h
+//  ProjectBat
+//
+//  Created by prayanalog on 2017. 7. 14..
+//  Copyright © 2017년 prayanalog. All rights reserved.
+//
+
+#ifndef BrigingHeader_h
+#define BrigingHeader_h
+
+#import <KakaoOpenSDK/KakaoOpenSDK.h>
+
+
+#endif /* BrigingHeader_h */
--- ProjectBat/ViewController.swift
+++ ProjectBat/ViewController.swift
@@ -20,6 +20,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func loginKakao(_ sender: UIButton) {
+        let session = KOSession.shared()
+        // 로그인 세션이 생성 되었으면
+        if let s = session {
+            // 이전 열린 세션은 닫고
+            if s.isOpen() {
+                s.close()
+            }
+            s.open(completionHandler: { (error) in
+                // 에러가 없으면
+                if error == nil {
+                    print("No error")
+                    // 로그인 성공
+                    if s.isOpen() {
+                        print("Success")
+                    }
+                    
+                    // 로그인 실패
+                    else {
+                        print("Fail")
+                    }
+                }
+                
+                // 로그인 에러
+                else {
+                    print("Error login: \(error!)")
+                }
+            })
+        }
+        // 로그인 세션 생성 실패
+        else {
+            print("Something wrong")
+
+        }
+    }
 
 }
 
